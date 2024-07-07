7591f412b734e6180dfd9b67c0c3dbfe5269558f
Anix <lega.kirill@mail.ru>
2024-07-07 21:26:21 +0300

    3 test

--- src/main/java/ru/Kirill/tgBot/repository/CategoryRepository.java
+++ src/main/java/ru/Kirill/tgBot/repository/CategoryRepository.java
@@ -1,11 +1,12 @@
 package ru.Kirill.tgBot.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.Kirill.tgBot.entity.Category;
 
-@RepositoryRestResource(collectionResourceRel = "category", path = "category")
-public interface CategoryRepository extends JpaRepository<Category, Long>
+
+public interface CategoryRepository extends CrudRepository<Category, Long>
 {
 
 }
